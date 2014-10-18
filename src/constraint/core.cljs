fd472d5deae1d5184f2c9d1923d73df7718fd19c
Mark Rogalski <interesting.ones.are.already.taken@gmail.com>
2014-10-18 14:48:45 +0200

    Drawing lines.

--- src/constraint/core.cljs
+++ src/constraint/core.cljs
@@ -1,33 +1,40 @@
 (ns constraint.core
   (:require [dommy.core :as dommy]
-            [crate.core :as crate]))
+            [crate.core :as crate]
+            [clojure.string :as string]))
+
+(def triangle-marker
+  [:svg:marker
+   {:id "Triangle"
+    :viewBox "0 0 10 10"
+    :refX "1"
+    :refY "5"
+    :markerWidth "6"
+    :markerHeight "6"
+    :orient "auto"}
+   [:svg:path {:d "M0,0 L 10,5 L0,10 z"}]]
+  )
+
+(defn make-line-dirs [from to]
+  (let [middle (map (comp #(/ % 2) +) from to)
+        coords (map (partial string/join ",") [from middle to])]
+    (string/join " " (map str ["M" "L" "L"] coords))))
+
+
+(defn line [from to color]
+  [:svg:path
+   {:d (make-line-dirs from to)
+    :stroke color
+    :stroke-width "4px"
+    :fill "none"
+    :marker-mid "url(#Triangle)"}])
+
 
 (.appendChild (dommy.core/sel1 :.forsvg)
-              (crate/html [:svg:svg
-                           {:width 1000 :height 1000}
-                           [:svg:defs
-                            [:svg:marker
-                             {:id "Triangle"
-                              :viewBox "0 0 10 10" 
-                              :refX "1"
-                              :refY "5"
-                              :markerWidth "6"
-                              :markerHeight "6"
-                              :orient "auto"}
-                             [:svg:path {:d "M0,0 L 10,5 L0,10 z"}]]
-                            ]
-                           [:svg:path
-                            {:d "M100,20 L150,20 L200,20"
-                             :stroke "#6666ff"
-                             :stroke-width "4px"
-                             :fill "none"
-                             :marker-mid "url(#Triangle)"}]
-                           [:svg:path
-                            {:d "M200,40 L150,60 L100,80"
-                             :stroke "#6666ff"
-                             :stroke-width "4px"
-                             :fill "none"
-                             :marker-mid "url(#Triangle)"}]
+              (crate/html [:svg:svg {:width 1000 :height 1000}
+                           [:svg:defs triangle-marker]
+                           (line [100, 20] [200, 40] "#6666ff")
+                           (line [200, 40] [100, 80] "red")
                            [:svg:circle {:id "button"
                                          :fill "yellow"
                                          :stroke "green"
