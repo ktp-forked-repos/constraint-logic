faf8be3963bf9d38989aca28527d06eec92fd7de
Mark Rogalski <interesting.ones.are.already.taken@gmail.com>
2014-10-18 15:13:54 +0200

    Add clickable lines.

--- src/constraint/core.cljs
+++ src/constraint/core.cljs
@@ -9,8 +9,8 @@
     :viewBox "0 0 10 10"
     :refX "1"
     :refY "5"
-    :markerWidth "6"
-    :markerHeight "6"
+    :markerWidth "3"
+    :markerHeight "3"
     :orient "auto"}
    [:svg:path {:d "M0,0 L 10,5 L0,10 z"}]]
   )
@@ -23,9 +23,10 @@
 
 (defn line [from to color]
   [:svg:path
-   {:d (make-line-dirs from to)
+   {:id "clickthis"
+    :d (make-line-dirs from to)
     :stroke color
-    :stroke-width "4px"
+    :stroke-width "10px"
     :fill "none"
     :marker-mid "url(#Triangle)"}])
 
@@ -42,14 +43,21 @@
                                          :cx 50 :cy 50
                                          :r 50}]]))
 
+(defn e->tid [e]
+  (-> e
+      (js->clj)
+      (.-target)
+      (.-id)))
+
 (defn click-handler [e]
+  (.log js/console (e->tid e))
   (js/alert "123"))
 
 
 
-(dommy.core/listen! (dommy.core/sel1 :circle) :click click-handler)
+(map #(dommy.core/listen! % :click click-handler) (dommy.core/sel :#clickthis))
 
-(dommy.core/append (dommy.core/parent (dommy.core/sel1 :circle))
-                   (crate/html
-                     [:svg:line]))
+; (dommy.core/append (dommy.core/parent (dommy.core/sel1 :circle))
+;                    (crate/html
+;                      [:svg:line]))
 
