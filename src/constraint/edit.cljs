0edbfed3a87d5cf0d7c85f6422bc8216d176d9fc
Mark Rogalski <interesting.ones.are.already.taken@gmail.com>
2014-11-13 11:50:02 +0100

    Add adding new vertices.

--- src/constraint/edit.cljs
+++ src/constraint/edit.cljs
@@ -2,7 +2,6 @@
   (:require [constraint.common :refer [edge-id
                                        vert-id]]))
 
-(def edge-id-len (count edge-id))
 (def vertex-regex (re-pattern (str vert-id ".*")))
 (def edge-regex (re-pattern (str edge-id ".*")))
 
@@ -24,24 +23,31 @@
 
 
 
-(defn get-key [edge-str]
+(defn get-key [len edge-str]
   (->> edge-str
-       (drop edge-id-len)
+       (drop len)
        (apply str)
        (js/parseInt)))
 
 
 
-(defn largest-key [{:keys [edges]}]
-  (-> (map get-key (keys edges))
-      (sort)
-      (reverse)
-      (first)))
+(defn largest-key [id coll]
+  (let [get-number-part (partial get-key (count id))]
+    (-> (map get-number-part (keys coll))
+        (sort)
+        (reverse)
+        (first))))
 
 
 
-(defn next-key [key-num]
-  (str edge-id (inc key-num)))
+(defn inc-key [id key-num]
+  (str id (inc key-num)))
+
+
+
+(defn next-key [id coll]
+  (inc-key id (largest-key id coll)))
+
 
 
 
@@ -53,9 +59,9 @@
 
 
 
-(defn make-new-edge [from to world-state]
-  (let [next-to-largest-key (next-key (largest-key world-state))]
-    [next-to-largest-key [from to :red]]))
+(defn make-new-edge [from to {:keys [edges]}]
+  (let [new-key (next-key edge-id edges)]
+    [new-key [from to :red]]))
 
 
 
@@ -105,13 +111,26 @@
 
 
 
-(defn handle-unslected [clicked-what world-state]
+(defn make-new-vertex [where {:keys [vertices]}]
+  (let [new-key (next-key vert-id vertices)]
+    [new-key [1 where]]))
+
+
+
+(defn add-vertex [event world-state]
+  (let [where (where-svg-was-clicked event)
+        new-vertex (make-new-vertex where world-state)]
+    (update-in world-state [:vertices] #(conj % new-vertex))))
+
+
+
+(defn handle-unslected [clicked-what event world-state]
   (let [clicked-edge? (re-matches edge-regex clicked-what)
         clicked-vertex? (re-matches vertex-regex clicked-what)]
     (cond
         clicked-edge? (toggle-edge-value world-state clicked-what)
         clicked-vertex? (select-vertex world-state clicked-what)
-        :else world-state)))
+        :else (add-vertex event world-state))))
 
 
 
@@ -119,4 +138,4 @@
   (let [unselect #(merge % {:selected nil})]
     (if (:selected world-state)
       (unselect (handle-selected clicked-what event world-state))
-      (handle-unslected clicked-what world-state))))
+      (handle-unslected clicked-what event world-state))))
